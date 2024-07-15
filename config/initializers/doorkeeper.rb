6b58df4cde27d332d9789aaeac5e21cbd2eca6b4
Mayurkumar Patel <mayurt20@gmail.com>
2024-07-15 14:23:30 +0530

    adds refresh token logic

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -18,8 +18,9 @@ Doorkeeper.configure do
   client_credentials :from_basic, :from_params
 
   access_token_expires_in 1.hour
+  use_refresh_token
 
-  grant_flows %w[password client_credentials]
+  grant_flows %w[password client_credentials refresh_token]
 
   default_client = Doorkeeper::Application.find_by(uid: 'ZK9kWWyutt3KlC0tMpBKa7XQNlMRwXbIHUVi_MN5ZMQ')
   default_client&.secret = 'esxMsb7maq2wv0lgsT8-uimUrhNQjs3Wp7x5Lvs1bAs' if default_client
